715cfddc28c6a013d9471b41c8e0abe64cdd1484
OvidiuDobran <dobran_ovi@yahoo.com>
2018-04-04 17:01:29 +0300

    Think I finished the LexicalAnalyser. Only needs testing.

--- src/com/ovi/analyser/LexicalAnalyser.java
+++ src/com/ovi/analyser/LexicalAnalyser.java
@@ -26,6 +26,7 @@ public class LexicalAnalyser {
 		removeEmptyLines();
 		printCodeLines();
 		getTokens();
+		System.out.println(tokens);
 	}
 
 	private void getTokens() {
@@ -229,6 +230,9 @@ public class LexicalAnalyser {
 				} else if (!"\\\\".contains(line.charAt(index) + "")) {
 					index++;
 					state = 15;
+				} else {
+					index++;
+					state = 53;
 				}
 				break;
 			case 14:
@@ -292,6 +296,186 @@ public class LexicalAnalyser {
 				String valueString = line.substring(startIndex, index);
 				RealToken tkString = new RealToken(AL.CT_STRING, lineIndex, Integer.parseInt(valueString));
 				tokens.add(tkString);
+				state = 0;
+				break;
+			case 23:
+				state = 26;
+				break;
+			case 26:
+				Token tkDiv = new Token(AL.DIV, lineIndex);
+				tokens.add(tkDiv);
+				state = 0;
+				break;
+			case 27:
+				Token tkAdd = new Token(AL.ADD, lineIndex);
+				tokens.add(tkAdd);
+				state = 0;
+				break;
+			case 28:
+				Token tkSub = new Token(AL.SUB, lineIndex);
+				tokens.add(tkSub);
+				state = 0;
+				break;
+			case 29:
+				Token tkMul = new Token(AL.MUL, lineIndex);
+				tokens.add(tkMul);
+				state = 0;
+				break;
+			case 30:
+				Token tkDot = new Token(AL.DOT, lineIndex);
+				tokens.add(tkDot);
+				state = 0;
+				break;
+			case 31:
+				Token tkAnd = new Token(AL.AND, lineIndex);
+				tokens.add(tkAnd);
+				state = 0;
+				break;
+			case 32:
+				if (line.charAt(index) == '&') {
+					index++;
+					state = 31;
+				}
+				break;
+			case 33:
+				if (line.charAt(index) == '|') {
+					index++;
+					state = 34;
+				}
+				break;
+			case 34:
+				Token tkOr = new Token(AL.OR, lineIndex);
+				tokens.add(tkOr);
+				state = 0;
+				break;
+			case 35:
+				if (line.charAt(index) == '=') {
+					index++;
+					state = 37;
+				} else {
+					index++;
+					state = 36;
+				}
+				break;
+			case 36:
+				Token tkNot = new Token(AL.NOT, lineIndex);
+				tokens.add(tkNot);
+				state = 0;
+				break;
+			case 37:
+				Token tkNotEq = new Token(AL.NOTEQ, lineIndex);
+				tokens.add(tkNotEq);
+				state = 0;
+				break;
+			case 38:
+				if (line.charAt(index) == '=') {
+					index++;
+					state = 40;
+				} else {
+					index++;
+					state = 39;
+				}
+				break;
+			case 39:
+				Token tkAssign = new Token(AL.ASSIGN, lineIndex);
+				tokens.add(tkAssign);
+				state = 0;
+				break;
+			case 40:
+				Token tkEq = new Token(AL.EQUAL, lineIndex);
+				tokens.add(tkEq);
+				state = 0;
+				break;
+			case 41:
+				if (line.charAt(index) == '=') {
+					index++;
+					state = 43;
+				} else {
+					index++;
+					state = 42;
+				}
+				break;
+			case 42:
+				Token tkLess = new Token(AL.LESS, lineIndex);
+				tokens.add(tkLess);
+				state = 0;
+				break;
+			case 43:
+				Token tkLEq = new Token(AL.LESSEQ, lineIndex);
+				tokens.add(tkLEq);
+				state = 0;
+				break;
+			case 44:
+				if (line.charAt(index) == '=') {
+					index++;
+					state = 46;
+				} else {
+					index++;
+					state = 45;
+				}
+				break;
+			case 45:
+				Token tkGre = new Token(AL.GREATER, lineIndex);
+				tokens.add(tkGre);
+				state = 0;
+				break;
+			case 46:
+				Token tkGEq = new Token(AL.GREATEREQ, lineIndex);
+				tokens.add(tkGEq);
+				state = 0;
+				break;
+			case 53:
+				Token tkComma = new Token(AL.COMMA, lineIndex);
+				tokens.add(tkComma);
+				state = 0;
+				break;
+			case 54:
+				Token tkSemic = new Token(AL.SEMICOLON, lineIndex);
+				tokens.add(tkSemic);
+				state = 0;
+				break;
+			case 55:
+				Token tkLPar = new Token(AL.LPAR, lineIndex);
+				tokens.add(tkLPar);
+				state = 0;
+				break;
+			case 56:
+				Token tkRPar = new Token(AL.RPAR, lineIndex);
+				tokens.add(tkRPar);
+				state = 0;
+				break;
+			case 57:
+				Token tkLBra = new Token(AL.LBRACKET, lineIndex);
+				tokens.add(tkLBra);
+				state = 0;
+				break;
+			case 58:
+				Token tkRBra = new Token(AL.RBRACKET, lineIndex);
+				tokens.add(tkRBra);
+				state = 0;
+				break;
+			case 59:
+				Token tkLAcc = new Token(AL.LACC, lineIndex);
+				tokens.add(tkLAcc);
+				state = 0;
+				break;
+			case 60:
+				Token tkRAcc = new Token(AL.RACC, lineIndex);
+				tokens.add(tkRAcc);
+				state = 0;
+				break;
+			case 61:
+				if(Character.isLetter(line.charAt(index))||Character.isDigit(line.charAt(index))||line.charAt(index)=='_') {
+					index++;
+					state=61;
+				}else {
+					index++;
+					state=62;
+				}
+			case 62:
+				String valueID=line.substring(startIndex,index);
+				StringToken tkID = new StringToken(AL.ID, lineIndex, valueID);
+				tokens.add(tkID);
 				state = 0;
 				break;
 			case 66:
